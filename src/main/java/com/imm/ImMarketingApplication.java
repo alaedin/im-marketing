10a5ced1dacf933efd43ebc0ba655af6b07202ed
root <arfaoui.ala.eddin@gmail.com>
2020-08-08 08:51:27 +0100

    before formatting

--- src/main/java/com/imm/ImMarketingApplication.java
+++ src/main/java/com/imm/ImMarketingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImMarketingReportingApplication {
+public class ImMarketingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImMarketingReportingApplication.class, args);
+		SpringApplication.run(ImMarketingApplication.class, args);
 	}
 
 }
