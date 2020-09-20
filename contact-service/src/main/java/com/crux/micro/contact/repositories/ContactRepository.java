3fe65318bdac603c65ca17786d6ac4ccbfcc47a8
alaedin <arfaoui.ala.eddin@gmail.com>
2020-09-20 12:20:48 +0100

    delete unused service

--- contact-service/src/main/java/com/crux/micro/contact/repositories/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crux.micro.contact.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.crux.micro.contact.entities.Contact;
-
-public interface ContactRepository extends JpaRepository<Contact, Integer> {
-
-}
